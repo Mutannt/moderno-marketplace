c072f0eb27653613e97519520e0bd89e48185a2a
Мурат Биктимиров <Bicmur@mail.ru>
2021-09-01 10:36:57 +0500

    added sections products and product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,18 @@
 // $ - Сначала html
 $(function(){
-    
+
+    $(".rate-star").rateYo({
+        rating: 3.5,
+        starWidth: "12px",
+        readOnly: true
+    });
+
+    $(".product-slider__inner").slick({ // slick слайдер
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true,
+        arrows: false,
+    });
+
+    var mixer = mixitup('.products__inner-box');
 });
